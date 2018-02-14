18c7b9eeba199d0e6a142cbcfd3ee35a1f48c230
pogorelov-aa <pogorelov_vt@mail.ru>
2018-02-15 02:23:24 +0500

    Вид личного кабинета (тестовый)

---
+++ views/user/lk.php
@@ -0,0 +1,26 @@
+<div class="user_main center">
+    <div id="left_user_block">
+        <div id="user_avatar_block">
+            <img id="user_photo" src="../../web/images/ava1.jpg" alt="user photo">
+        </div>
+        Борис
+        <hr> Мои кредиты<br> Всего: 2
+    </div>
+    <div id="user_content">
+       <h2>Это личный кабинет пользователя</h2>
+        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In, perferendis laudantium molestias voluptates eius modi dolorem fuga, officia atque et rem a alias eligendi doloremque adipisci magni, aliquam, omnis quis.</div>
+        <div>Pariatur eius, nemo, incidunt in ullam quidem magnam ipsa voluptatibus beatae sit eum saepe at vero libero eveniet esse distinctio sunt dolore repudiandae vitae doloribus tempore. In excepturi laboriosam, deleniti.</div>
+        <div>Molestiae, quasi sequi. Nulla harum, omnis nostrum expedita sequi totam fugit velit aut inventore consequuntur, libero dolorem non nobis tempore! Neque quasi soluta, debitis minus sit. Labore sit itaque inventore?</div>
+        <div>Beatae perspiciatis, odit alias fuga necessitatibus magnam incidunt odio nulla laboriosam autem quisquam, neque dolorum et unde quis reiciendis consequuntur iste ut fugit in reprehenderit sapiente aliquam non quasi. Earum!</div>
+        <div>Eligendi nihil officia, delectus ex ea voluptatem voluptatum iusto veniam quibusdam aut ad facere. Dolorum velit a fugit deserunt accusantium tenetur labore quod voluptatum, aspernatur magnam doloribus eum! Minus, enim.</div>
+        <div>Repudiandae est pariatur impedit temporibus labore assumenda voluptatibus ipsum ab error similique voluptatum neque explicabo odit, debitis aliquid fugit sequi earum dolorum, quidem autem libero aspernatur velit ad. Quasi, amet.</div>
+        <div>Soluta voluptas quod itaque perferendis ipsum aliquid architecto praesentium voluptatibus qui omnis, repellat amet, nisi libero! Quos aut, facere iure soluta ad laudantium aliquam doloremque ab quisquam quis, distinctio fugit!</div>
+        <div>Eum consectetur rerum labore sequi consequuntur, necessitatibus error in nam id porro ab quam voluptas, at. Quibusdam vero minus, debitis totam autem tempore consequatur ut, voluptates, odit perferendis minima nisi.</div>
+        <div>Quia asperiores eaque deleniti reprehenderit. Laboriosam aperiam, laborum delectus blanditiis? Eum tempore quasi illum distinctio tenetur eius quam, consequuntur earum. At animi, porro laborum maiores asperiores ad, laudantium explicabo reiciendis.</div>
+        <div>Ipsa, ea iure dolorum eius consequatur vel quo veritatis animi in explicabo quas quasi quaerat quod sapiente qui rerum. Totam illum quaerat laborum iusto, a velit ipsum beatae soluta quasi.</div>
+        <div>Neque, itaque laboriosam animi et unde assumenda ex? Eos expedita consequuntur iste unde sequi doloribus at velit aliquid quibusdam dolorum architecto illum, quaerat. Ipsam consequuntur laborum inventore placeat officiis impedit.</div>
+
+
+    </div>
+
+</div>
