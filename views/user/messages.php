e4caab7c37904ab436a009e5d642e9ef5d255339
pogorelov-aa <pogorelov_vt@mail.ru>
2018-02-16 03:23:20 +0500

    Вид Сообщения пользователя

---
+++ views/user/messages.php
@@ -0,0 +1,14 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Сообщения';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Сообщения</h1>
+
+
+    </div>
+</div>
