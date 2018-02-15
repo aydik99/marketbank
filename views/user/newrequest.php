b7024d31bd19fe6059768b630a1274b7e3ab471c
pogorelov-aa <pogorelov_vt@mail.ru>
2018-02-16 03:23:32 +0500

    Вид Новый запрос на кредит

---
+++ views/user/newrequest.php
@@ -0,0 +1,14 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Подать заявку';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Подать заявку</h1>
+
+
+    </div>
+</div>
