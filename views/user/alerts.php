3a0d938149266d9b3aece962033eb82a6ee527fc
pogorelov-aa <pogorelov_vt@mail.ru>
2018-02-16 03:23:01 +0500

    Вид Уведомления

---
+++ views/user/alerts.php
@@ -0,0 +1,14 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Уведомления';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Уведомления</h1>
+
+
+    </div>
+</div>
