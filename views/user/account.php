b6e2197ef3789571a6fbbbec2dbed0d8c99696ae
pogorelov-aa <pogorelov_vt@mail.ru>
2018-02-16 03:24:54 +0500

    Вид Профиль пользователя

---
+++ views/user/account.php
@@ -0,0 +1,14 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Профиль';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Профиль пользователя</h1>
+
+
+    </div>
+</div>
