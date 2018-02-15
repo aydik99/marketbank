05e3e5a4952cb958278f4a685541fc5a10c8e197
pogorelov-aa <pogorelov_vt@mail.ru>
2018-02-16 03:23:10 +0500

    Вид Мои кредиты

---
+++ views/user/credits.php
@@ -0,0 +1,14 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Мои кредиты';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Кредиты пользователя</h1>
+
+
+    </div>
+</div>
