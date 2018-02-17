422f0a4254f6f146ecb54e700541fe029a6ba506
pogorelov-aa <pogorelov_vt@mail.ru>
2018-02-18 03:19:27 +0500

    В конфиг добавлены данные e-mail администратора

--- config/web.php
+++ config/web.php
@@ -32,7 +32,16 @@ $config = [
             // send all mails to a file by default. You have to set
             // 'useFileTransport' to false and configure a transport
             // for the mailer to send real emails.
-            'useFileTransport' => true,
+            'useFileTransport' => false,
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.yandex.ru',
+                'username' => 'marketbank',
+                'password' => 'Qwerty123',
+                'port' =>'465',
+                'encryption' => 'ssl',
+            ]
+            
         ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
