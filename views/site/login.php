eb159cf3065380678a73381bed7e27852f252e2d
pogorelov-aa <pogorelov_vt@mail.ru>
2018-02-18 03:22:12 +0500

    мелкие исправления формы входа

--- views/site/login.php
+++ views/site/login.php
@@ -11,51 +11,55 @@ $tagline = 'Биржа финансово-банковских услуг';
 $this->title = 'Войти';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-login">
+    <div class="site-login">
 
-    <div class="login-link-wrapper text-center">
-        <a 
-            href="<?=Yii::$app->urlManager->CreateUrl(['site/index']) ?>"
-            class="login-site-link"
-        >
-            <img src="/images/marketbank.png" alt="marketbank logo" class="login-logo">
-            <span>market</span><span>bank</span>
-        </a>
-    </div>
+        <div class="login-link-wrapper text-center">
+            <a href="<?=Yii::$app->urlManager->CreateUrl(['site/index']) ?>" class="login-site-link">
+                <img src="/images/marketbank.png" alt="marketbank logo" class="login-logo">
+                <span>market</span><span>bank</span>
+            </a>
+        </div>
 
-    <h1 class="login-tagline text-center"><?= Html::encode($tagline) ?></h1>
+        <h1 class="login-tagline text-center">
+            <?= Html::encode($tagline) ?>
+        </h1>
 
-    <?php $form = ActiveForm::begin([
+        <?php $form = ActiveForm::begin([
         'id' => 'login-form',
         'layout' => 'horizontal',
+        'validateOnBlur'         => false,
         'fieldConfig' => [
             'template' => "{label}\n<div class=\"col-lg-8\">{input}</div>\n<div class=\"col-lg-12\">{error}</div>",
-            'labelOptions' => ['class' => 'col-lg-4 control-label']
-        ],
+            'labelOptions' => ['class' => 'col-lg-4 control-label'],
+        ],            
     ]); ?>
 
-        <h2 class="text-center"><?= Html::encode($this->title) ?></h2>
+        <h2 class="text-center">
+            <?= Html::encode($this->title) ?>
+        </h2>
 
         <?= $form->field($model, 'username')->textInput(['autofocus' => true])->label('Логин') ?>
 
-        <?= $form->field($model, 'password')->passwordInput()->label('Пароль') ?>
+            <?= $form->field($model, 'password')->passwordInput()->label('Пароль') ?>
 
-        <?= $form->field($model, 'rememberMe')->checkbox([
+                <?= $form->field($model, 'rememberMe')->checkbox([
             'template' => "<div class=\"col-lg-12\">{input} {label}</div>\n<div class=\"col-lg-12\">{error}</div>",
         ])->label('Запомнить меня') ?>
 
-        <div class="form-group">
-            <div class="col-lg-12">
-                <?= Html::submitButton('Войти', ['class' => 'btn btn-primary btn-lg btn-block', 'name' => 'login-button']) ?>
-            </div>
-        </div>
-        
-        <div class="text-center login-err-message"><?=$model->err_message ?></div>
+                    <div class="form-group">
+                        <div class="col-lg-12">
+                            <?= Html::submitButton('Войти', ['class' => 'btn btn-primary btn-lg btn-block', 'name' => 'login-button']) ?>
+                        </div>
+                    </div>
 
-        <div class="text-center" style="color:#999;">
-            Ещё не зарегестрированы <a href="<?=Yii::$app->urlManager->CreateUrl(['site/register']) ?>">Регистрация</a>        
-        </div>
+                    <div class="text-center login-err-message">
+                        <?=$model->err_message ?>
+                    </div>
 
-    <?php ActiveForm::end(); ?>
+                    <div class="text-center" style="color:#999;">
+                        Ещё не зарегестрированы&#63; <a href="<?=Yii::$app->urlManager->CreateUrl(['site/register']) ?>">Регистрация</a>
+                    </div>
 
-</div>
+                    <?php ActiveForm::end(); ?>
+
+    </div>
