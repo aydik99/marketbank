43ae4bc893a1ef62911ca0d8e21c7cf09ee48c92
Александр Стекольщиков <AleksStek@mail.ru>
2018-03-16 05:29:29 +0700

    Добавление стилей к "подать заявку"

--- views/user/newrequest.php
+++ views/user/newrequest.php
@@ -14,12 +14,12 @@ $orderOptions = [
 
     <div class="col-md-9">
 
-        <form action="" class="bg-white">
+        <form action="" class="bg-white order-form">
             <fieldset>
-                <legend>Оформление заказа</legend>
+                <legend class="order-title order-title--main">Оформление заказа</legend>
 
-                <div class="form-group">
-                    <label for="ordertype" class="col-md-2">Вид</label>
+                <div class="form-group clearfix">
+                    <label for="ordertype" class="col-md-2 order-label">Вид</label>
                     <div class="col-md-10">
                         <select name="order" id="ordertype" class="form-control">
                         <?php
@@ -33,24 +33,24 @@ $orderOptions = [
                     </div>
                 </div>
 
-                <div class="form-group">
-                    <label for="amout" class="col-md-2">Сумма</label>
+                <div class="form-group clearfix">
+                    <label for="amout" class="col-md-2 order-label">Сумма</label>
                     <div class="col-md-10">
-                        <input type="number" id="amount" value="0" min="0" class="form-control">
+                        <input type="number" id="amount" value="0" min="0" class="form-control order-input">
                     </div>
                 </div>
 
-                <div class="form-group">
-                    <label for="description">Пояснение</label>
-                    <textarea name="" id="description" class="form-control"></textarea>
+                <div class="form-group order-description-block">
+                    <label for="description" class="order-description-title">Пояснение</label>
+                    <textarea name="" id="description" class="form-control order-description" rows="8"></textarea>
                 </div>
 
                 <div class="form-group">
-                    <label for="attachedFile">Прикрепить файл</label>
-                    <input type="file" id="attachedFile">
+                    <label for="attachedFile" class="btn btn-default order-file-btn">Прикрепить файл</label>
+                    <input type="file" id="attachedFile" class="hidden">
                 </div>
 
-                <button type="submit" class="btn btn-primary pull-right">Отправить</button>
+                <button type="submit" class="btn btn-primary btn-lg pull-right">Отправить</button>
 
             </fieldset>
         </form>
@@ -59,12 +59,12 @@ $orderOptions = [
 
     <div class="col-md-3">
         <div class="bg-white">
-            <h2>Шаги</h2>
+            <h2 class="order-title text-center">Шаги</h2>
 
-            <ul class="list-group">
-                <li class="list-group-item">Создание</li>
-                <li class="list-group-item">В аукционе</li>
-                <li class="list-group-item">Сделка</li>
+            <ul class="list-group order-step-group">
+                <li class="list-group-item order-step">Создание</li>
+                <li class="list-group-item order-step">В аукционе</li>
+                <li class="list-group-item order-step">Сделка</li>
             </ul>
         </div>
     </div>
