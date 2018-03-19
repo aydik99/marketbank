f150a26339c0a16213245c0e2963004172651153
pogorelov-aa <pogorelov_vt@mail.ru>
2018-03-20 02:11:50 +0500

    доработка страницы "Мои кредиты"

--- views/user/credits.php
+++ views/user/credits.php
@@ -4,51 +4,38 @@
 
 $this->title = 'Мои кредиты';
 ?>
-<div class="site-index center">
+    <div class="site-index center">
 
-    <div class="col-md-3">
+        <div class="col-md-3">
 
-        <ul class="list-group">
-            <li class="list-group-item active">Мои кредиты</li>
-            <li class="list-group-item">В аукционе</li>
-            <li class="list-group-item">Активные</li>
-            <li class="list-group-item">Завершённые</li>
-        </ul>
+            <ul class="list-group">
+                <li class="list-group-item active" data-filter="0">Все кредиты</li>
+                <li class="list-group-item" data-filter="1">В аукционе</li>
+                <li class="list-group-item" data-filter="2">Активные</li>
+                <li class="list-group-item"  data-filter="3">Завершённые</li>
+            </ul>
 
-    </div>
+        </div>
 
-    <div class="col-md-9">
+        <div class="col-md-9">
+            <? foreach ($ask as $data): ?>
+               
+                <section class="credit-item" data-status="<?=$data->status; ?>">
+                    <header class="credit-item__header">
+                        <h3 class="text-center"><?=$data->asktype->name_type;?>&nbsp;
+                        &#40;<?=$data->summa;?>&nbsp;руб&#41;</h3>
+                    </header>
 
-        <section class="credit-item">
-            <header class="credit-item__header">
-                <h3 class="text-center">Ипотека</h3>
-            </header>
+                    <p class="credit-item__content">                        
+                        <?= $data->comment; ?>
+                    </p>
 
-            <p class="credit-item__content">
-                Неодходимо кредит в 400000 руб. Работаю в компании газпром.
-            </p>
-
-            <footer class="credit-item__footer">
-                Подано: <time>21.12.2017</time>
-                <span class="pull-right">9 предложений</span>
-            </footer>
-        </section>
-
-        <section class="credit-item">
-            <header class="credit-item__header">
-                <h3 class="text-center">Ипотека</h3>
-            </header>
-
-            <p class="credit-item__content">
-                Неодходимо кредит в 400000 руб. Работаю в компании газпром.
-            </p>
-
-            <footer class="credit-item__footer">
-                Подано: <time>21.12.2017</time>
-                <span class="pull-right">9 предложений</span>
-            </footer>
-        </section>
+                    <footer class="credit-item__footer">
+                        Подано: <time><?=$data->date_start ?></time>
+                        <span class="pull-right">9 предложений</span>
+                    </footer>
+                </section>
+             <? endforeach; ?>
+        </div>
 
     </div>
-    
-</div>
