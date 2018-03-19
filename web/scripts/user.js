3156a57c7819f526c9576b7e0d88159ae00479a1
pogorelov-aa <pogorelov_vt@mail.ru>
2018-03-20 02:14:46 +0500

    работа меню на странице "мои кредиты"

--- web/scripts/user.js
+++ web/scripts/user.js
@@ -2,4 +2,19 @@ $(function() {
     $('.dropdown').on('click', function () {
         $(this).toggleClass('open');
     });
+    
+    $(".credits .list-group-item").on("click", function(){
+        $(".credits .active").removeClass('active');
+        $(this).addClass('active');
+        let stat = $(this).attr('data-filter');
+        if (stat>0) {
+            $('.credit-item[data-status != '+stat+']').hide();
+            $('.credit-item[data-status = '+stat+']').show();    
+        } else {
+            $('.credit-item').show();    
+        }
+        
+    });
+    
+    
 })
